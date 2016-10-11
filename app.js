61648f9cbfa7ef13d3ab708b1d597482ef9044b8
swkim <kim2450@naver.com>
2016-10-11 15:40:19 +0900

    make it better

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
  
-var server = app.listen(3000, function(){
+var server = app.listen(8080, function(){
     console.log("Express server has started on port 3000")
 });
 
