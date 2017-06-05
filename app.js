7149a46b0a47cd3abf68d037d2b25bc33e0bcea9
swkim <kim2450@naver.com>
2017-06-05 17:03:35 +0900

    initial Commit

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
  
-var server = app.listen(8080, function(){
+var server = app.listen(9999, function(){
     console.log("Express server has started on port 8080")
 });
 
